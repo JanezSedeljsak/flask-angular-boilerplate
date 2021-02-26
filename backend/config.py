cf00f41e4c1fb6068060123946a735dce20da988
JanezSedeljsak <janez.sedeljsak@gmail.com>
2021-02-26 16:22:38 +0100

    auto create db

--- backend/config.py
+++ backend/config.py
@@ -1,5 +1,12 @@
 # I suggest putting this into env variables for security reasons
-DB_URI = 'mysql://root:@localhost/example'
+DB_CONFIG = {
+    "USERNAME": "root",
+    "PASSWORD": "",
+    "URL": "localhost",
+    "DB_NAME": "cars"
+}
+
+DB_URI = 'mysql://%s:%s@%s/%s' % (DB_CONFIG["USERNAME"], DB_CONFIG["PASSWORD"], DB_CONFIG["URL"], DB_CONFIG["DB_NAME"])
 
 # configuration object for Flask application
 class Config(object):
--- backend/models.py
+++ backend/models.py
@@ -1,10 +1,11 @@
 from sqlalchemy import create_engine, Column, Integer, String, MetaData, Table, DateTime
 from datetime import datetime
+import MySQLdb as mySQL
 
 try:
-    from .config import DB_URI
+    from .config import DB_URI, DB_CONFIG
 except:
-    from config import DB_URI
+    from config import DB_URI, DB_CONFIG
 
 metadata = MetaData()
 
@@ -17,5 +18,9 @@ notes = Table('notes', metadata,
 )
 
 # methods which should be called on the init of the database
+db = mySQL.connect(host=DB_CONFIG["URL"],user=DB_CONFIG["USERNAME"],passwd=DB_CONFIG["PASSWORD"])
+db1 = db.cursor()
+db1.execute("CREATE DATABASE IF NOT EXISTS %s" % DB_CONFIG["DB_NAME"])
 engine = create_engine(DB_URI)
+    
 metadata.create_all(engine)
